abedab3582c7d0edff57a9b612725bb97a98089b
User <User@HOME-PC>
2024-12-16 03:50:50 +1000

    Добавьте файлы проекта.

---
+++ Планировщик Подарков/Form2.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Планировщик_Подарков
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textLastName = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textPatronymicName = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textFirstName = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textDR = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            textBox12 = new TextBox();
+            label12 = new Label();
+            label13 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textLastName
+            // 
+            textLastName.Location = new Point(12, 82);
+            textLastName.Name = "textLastName";
+            textLastName.Size = new Size(351, 23);
+            textLastName.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 223);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(351, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(1025, 181);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(351, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(1025, 131);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(351, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(1025, 82);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(351, 23);
+            textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(571, 181);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(351, 23);
+            textBox6.TabIndex = 5;
+            // 
+            // textPatronymicName
+            // 
+            textPatronymicName.Location = new Point(12, 164);
+            textPatronymicName.Name = "textPatronymicName";
+            textPatronymicName.Size = new Size(351, 23);
+            textPatronymicName.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(571, 131);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(351, 23);
+            textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(571, 82);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(351, 23);
+            textBox9.TabIndex = 8;
+            // 
+            // textFirstName
+            // 
+            textFirstName.Location = new Point(12, 123);
+            textFirstName.Name = "textFirstName";
+            textFirstName.Size = new Size(351, 23);
+            textFirstName.TabIndex = 9;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 15);
+            label1.TabIndex = 10;
+            label1.Text = "ФИО";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 205);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 15);
+            label2.TabIndex = 11;
+            label2.Text = "Описание";
+            // 
+            // textDR
+            // 
+            textDR.Location = new Point(12, 296);
+            textDR.Name = "textDR";
+            textDR.Size = new Size(351, 23);
+            textDR.TabIndex = 12;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 15);
+            label3.TabIndex = 13;
+            label3.Text = "Человек";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 278);
+            label4.Name = "label4";
+            label4.Size = new Size(22, 15);
+            label4.TabIndex = 14;
+            label4.Text = "ДР";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(571, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Праздники";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(1025, 25);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 15);
+            label6.TabIndex = 16;
+            label6.Text = "Подарки";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(571, 64);
+            label7.Name = "label7";
+            label7.Size = new Size(59, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Название";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(571, 113);
+            label8.Name = "label8";
+            label8.Size = new Size(62, 15);
+            label8.TabIndex = 18;
+            label8.Text = "Описание";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(571, 163);
+            label9.Name = "label9";
+            label9.Size = new Size(32, 15);
+            label9.TabIndex = 19;
+            label9.Text = "Дата";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(1025, 64);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 15);
+            label10.TabIndex = 20;
+            label10.Text = "Название";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(1025, 113);
+            label11.Name = "label11";
+            label11.Size = new Size(35, 15);
+            label11.TabIndex = 21;
+            label11.Text = "Цена";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(1025, 223);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(351, 23);
+            textBox12.TabIndex = 22;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(1025, 163);
+            label12.Name = "label12";
+            label12.Size = new Size(65, 15);
+            label12.TabIndex = 23;
+            label12.Text = "праздники";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(1025, 207);
+            label13.Name = "label13";
+            label13.Size = new Size(27, 15);
+            label13.TabIndex = 24;
+            label13.Text = "чел";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1170, 326);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 79);
+            button1.TabIndex = 25;
+            button1.Text = "Сохранить и выйти";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1476, 792);
+            Controls.Add(button1);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(textBox12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textDR);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textFirstName);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textPatronymicName);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textLastName);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textLastName;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textPatronymicName;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textFirstName;
+        private Label label1;
+        private Label label2;
+        private TextBox textDR;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox textBox12;
+        private Label label12;
+        private Label label13;
+        private Button button1;
+    }
+}
---
+++ Планировщик Подарков/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Планировщик_Подарков
+{
+    public partial class Form2 : Form
+    {
+        private readonly DB db;
+        private Human human;
+        private Holidays holidays;
+        private Gifts gifts;
+        public Form2(DB db)
+        {
+            InitializeComponent();
+            this.db = db;
+            human = new Human();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            human.FirstName = textFirstName.Text;
+            human.LastName = textLastName.Text;
+            human.PatronymicName = textPatronymicName.Text;
+            human.DR = textDR.Text;
+            if (db != null)
+            {
+                db.AddHuman(human);
+                DialogResult = DialogResult.OK;
+                Close();
+
+
+
+            }
+        }
+    }
+}
